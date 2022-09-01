e4aa42d4db71888daa1ea5563f9788575926edad
João Victor da Silva <jv147002@gmail.com>
2022-09-01 16:50:41 -0300

    primeiro commit

---
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -0,0 +1,10 @@
+package br.senai.sp.jandira;
+
+public class AppTabuada {
+	
+	
+	
+	
+	
+
+}
---
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -0,0 +1,10 @@
+package br.senai.sp.jandira.model;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
